2554c88725e6cbb9a17a97a1bcd63665777a4206
rubinchase25 <chaserubin25@gmail.com>
2024-01-10 18:17:54 -0500

    Update eval7test.py

---
+++ eval7test.py
@@ -0,0 +1,55 @@
+import eval7, pprint
+from pprint import pprint
+import numpy as np
+
+deck = eval7.Deck()
+deck.shuffle()
+hand = deck.deal(5)
+#pprint.pprint(hand)
+  
+hand1 = [eval7.Card('2h'), eval7.Card('2h')]
+x = eval7.evaluate(hand1)
+# print(x)
+# print(eval7.handtype(x))
+
+hr = eval7.HandRange("89+")
+# print(hr.hands)
+
+# print(len(all_hands))
+
+# print(np.random.normal(1,.5))
+
+board = [eval7.Card(x) for x in ['9h', '6h', '5h', '2h', '4d']]
+my_hole = [eval7.Card(a) for a in ['8s', '7s']]
+comb = board + my_hole
+num_more_board = 5 - len(board)
+
+
+ya = False
+
+if len(my_hole) == 2 and ya:
+    opp_num = 3
+else:
+    opp_num = 2
+
+deck = eval7.Deck()
+for card in comb:
+    deck.cards.remove(card)
+
+num_better = 0
+trials = 0
+
+while trials < 500:
+    deck.shuffle()
+    cards = deck.peek(opp_num + num_more_board)
+    opp_hole = cards[:opp_num]
+    board_rest = cards[opp_num:]
+    print(opp_hole, board_rest)
+    my_val = eval7.evaluate(my_hole+board+board_rest)
+    opp_value = eval7.evaluate(opp_hole+board+board_rest)
+    if opp_value > my_val:
+        num_better += 1
+    trials += 1
+
+percent_better_than = 1 - (num_better/trials)
+print(percent_better_than)

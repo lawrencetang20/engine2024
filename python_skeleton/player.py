7604b69fdf1880e03697dca7033705c9a26c4f2f
lawrence tang <lawrence.j.tang@gmail.com>
2024-01-14 15:25:20 -0500

    new auction sizing

--- python_skeleton/player.py
+++ python_skeleton/player.py
@@ -326,9 +326,9 @@ class Player(Bot):
         need_auction, win_without, win_with = auction_strength
 
         if win_without < 0.2 or win_with < .6:
-            return BidAction(1)
+            return BidAction(min(my_stack, int(self.opp_total_bid/self.num_auctions_seen * 1/2)))
         elif win_without > 0.5:
-            return BidAction(int(self.auction_factor*need_auction*my_stack*2/3))
+            return BidAction(int(self.auction_factor*need_auction*my_stack*1/3))
         elif win_without <= 0.5 and win_without >= 0.2:
             return BidAction(int(self.auction_factor*need_auction*my_stack*1/4))
         else:
